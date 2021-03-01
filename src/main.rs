1580c1123039244f8ed5268a0d3d50f7e5c2c694
miro <furtadomiro@gmail.com>
2021-02-28 18:10:41 -0800

    initial features

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,36 @@ use std::env;
 use futures::StreamExt;
 use telegram_bot::*;
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    let token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
-    let api = Api::new(token);
+struct GroceryList {
+    entries: Vec<String>
+}
 
-    // Fetch new updates via long poll method
+impl GroceryList {
+    fn new() -> GroceryList {
+        GroceryList { entries: vec![] }
+    }
+
+}
+
+async fn execution_loop(mut list: GroceryList, api: &telegram_bot::Api) -> Result<(), Error> {
     let mut stream = api.stream();
     while let Some(update) = stream.next().await {
-        // If the received update contains a new message...
         let update = update?;
         if let UpdateKind::Message(message) = update.kind {
             if let MessageKind::Text { ref data, .. } = message.kind {
                 // Print received text message to stdout.
+                if data == "/view" {
+                    for element in &list.entries {
+                        api.send(message.text_reply(format!(
+                            "* {}",
+                            &element
+                        )))
+                        .await?; 
+                    }
+                }
                 println!("<{}>: {}", &message.from.first_name, data);
-
-                // Answer message with "Hi".
+                list.entries.push(data.clone());
+                // Answer message with "mHi".
                 api.send(message.text_reply(format!(
                     "Hi, {}! You just wrote '{}'",
                     &message.from.first_name, data
@@ -27,5 +41,16 @@ async fn main() -> Result<(), Error> {
             }
         }
     }
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    // setup API and GroceryList data structure
+    let token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
+    let api = Api::new(token);
+    let list = GroceryList::new();
+
+    execution_loop(list, &api).await?; // enter main execution loop
     Ok(())
 }
